dc88a1a6b058c6e79aa8d6b0fb04c6671c0b20dd
Wayne Manselle <wayne@viridianforge.tech>
2021-04-04 22:58:50 -0700

    WIP - more scaffolding

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-purple_exporter
+# purple_exporter
+
+purple_exporter is a basic Prometheus exporter that exports information from
+a specified series of PurpleAir sensors.
+
+## Requirements for Use
+
+Get a PurpleAir API key
+
+Configure environment variables
+
+## Use
+
+Launch the thing
+
+Use the data
+
+https://api.purpleair.com/#api-welcome
+
+## Sample Response
+{
+  "api_version" : "V1.0.6-0.0.9",
+  "time_stamp" : 1617597434,
+  "sensor" : {
+    "sensor_index" : REDACTED,
+    "name" : "REDACTED",
+    "model" : "PA-II",
+    "location_type" : 0,
+    "latitude" : REDACTED,
+    "longitude" : REDACTED,
+    "altitude" : REDACTED,
+    "last_seen" : 1617597338,
+    "last_modified" : 1612417482,
+    "private" : 0,
+    "channel_state" : 3,
+    "channel_flags_manual" : 0,
+    "humidity_a" : 44,
+    "temperature_a" : 48.0,
+    "pressure_a" : 1013.42,
+    "pm1.0_a" : 0.77,
+    "pm1.0_b" : 0.64,
+    "pm2.5_a" : 1.71,
+    "pm2.5_b" : 1.05,
+    "pm10.0_a" : 2.71,
+    "pm10.0_b" : 1.32,
+    "0.3_um_count_a" : 431.79,
+    "0.3_um_count_b" : 473.08,
+    "0.5_um_count_a" : 123.86,
+    "0.5_um_count_b" : 120.76,
+    "1.0_um_count_a" : 21.04,
+    "1.0_um_count_b" : 12.14,
+    "2.5_um_count_a" : 2.43,
+    "2.5_um_count_b" : 1.37,
+    "5.0_um_count_a" : 1.5,
+    "5.0_um_count_b" : 0.22,
+    "10.0_um_count_a" : 1.11,
+    "10.0_um_count_b" : 0.0,
+    "stats_a" : {"pm2.5":1.71,"pm2.5_10minute":0.54,"pm2.5_30minute":0.57,"pm2.5_60minute":0.98,"pm2.5_6hour":6.6,"pm2.5_24hour":9.98,"pm2.5_1week":7.98,"time_stamp":1617597338},
+    "stats_b" : {"pm2.5":1.05,"pm2.5_10minute":0.56,"pm2.5_30minute":0.57,"pm2.5_60minute":0.86,"pm2.5_6hour":5.82,"pm2.5_24hour":9.0,"pm2.5_1week":7.29,"time_stamp":1617597338},
+    "analog_input" : 0.05,
+    "primary_id_a" : REDACTED,
+    "primary_key_a" : "REDACTED",
+    "secondary_id_a" : REDACTED,
+    "secondary_key_a" : "REDACTED",
+    "primary_id_b" : REDACTED,
+    "primary_key_b" : "REDACTED",
+    "secondary_id_b" : REDACTED,
+    "secondary_key_b" : "REDACTED",
+    "hardware" : "2.0+BME280+PMSX003-B+PMSX003-A",
+    "led_brightness" : 15.0,
+    "firmware_upgrade" : "6.01",
+    "firmware_version" : "6.01",
+    "rssi" : -84.0,
+    "icon" : 0,
+    "confidence_manual" : 100,
+    "confidence_auto" : 100,
+    "channel_flags_auto" : 0
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,79 @@ use env_logger::{
 };
 
 use log::info;
-use prometheus_exporter::prometheus::register_gauge;
+use prometheus_exporter::prometheus::register_gauge_vec;
+use prometheus_exporter::prometheus::core::MetricVec;
 use std::net::SocketAddr;
+
+// Metrics - should be grabbed from a single API call. Calls should be limited
+// to only as often as Purple Requests (1-10 Minutes)
+// Make absolutely sure that requests for multiple sensors are fit into a single
+// request.
+
+// Configuration Required
+// API_READ_KEY
+// UPDATE_FREQUENCY (60s, 300s, 600s)
+// Humidity (sensor A only)
+// Temperature (sensor A only)
+// Pressure (sensor A only)
+// Analog_Input
+// SENSOR READINGS (For A and B)
+// pm1.0
+// pm2.5
+// pm10
+// 0.3_um_count
+// 0.5_um_count
+// 1.0_um_count
+// 2.5_um_count
+// 5.0_um_count
+// 10.0_um_count
+
+// Retrieves a reading from the PurpleAPI sensor API end point
+// The response from this is used to update the outputs of the exporter
+fn get_reading(sensor_index:String, purple_read_key:String) -> String{
+    // Get Metrics
+    let apiResponse = reqwest::blocking::get(&format!("https://api.purpleair.com/v1/sensors/{}", sensor_index))
+    .expect("Response from PurpleAPI could not be retrieved.")
+    .text(); //Needs to be JSON
+
+    //Need to convert this response into a proper form
+    apiResponse.unwrap()
+}
 
 fn main() {
     Builder::from_env(Env::default().default_filter_or("info")).init();
 
-    //Set up Address
+    //Set up configuration items
+    let update_frequency = std::time::Duration::from_secs(300);
+    let sensor_index = String::from("REDACTED");
+    let purple_read_key = String::from("REDACTED");
+
+
+    //Set up Exporter Address
     let raw_addr = "0.0.0.0:9184";
     let addr : SocketAddr = raw_addr.parse().expect("Listening Address Could Not Be Parsed.");
 
-    //Set up fake metric
-    let metric = register_gauge!("simple_the_answer", "to_everything")
-        .expect("Could not register 'simple_the_answer' as gauge.");
-
-    metric.set(42.0);
+    //Set up Metrics 
+    let env_gauge_vec = register_gauge_vec!("Environment_Gauge_Vec", "Sensor Environmental Data", &["Humidity", "Sensor Temp", "Pressure"])
+        .expect("Could not register environmental metrics as vec gauage.");
+    
+    let pm_gauge_vec = register_gauge_vec!("PM_Gauge_Vec", "Sensor Estimated Mass Concentrations (ug/m^3)", &["pm1.0_a", "pm2.0_a", "pm10.0_a","pm1.0_b", "pm2.0_b", "pm10.0_b"])
+        .expect("Could not register mass concentration metrics as vec gauage.");
+    
+    let um_gauge_vec = register_gauge_vec!("UM_Gauge_Vec", "Sensor Particle Counts by Size", &["0.3_um_a", "0.5_um_a", "1.0_um_a","2.5_um_a", "5.0_um_a", "10.0_um_b", "0.3_um_b", "0.5_um_b", "1.0_um_b","2.5_um_b", "5.0_um_b", "10.0_um_b"])
+        .expect("Could not register particle counts as vec gauage.");
 
     //Start exporter
-    prometheus_exporter::start(addr).expect("Cannot start exporter.");
+    let purple_exporter = prometheus_exporter::start(addr).expect("Cannot start exporter.");
 
-    // Get Metrics
-    let body = reqwest::blocking::get(&format!("http://{}/metrics", raw_addr))
-        .expect("Cannot retrieve metrics from exporter")
-        .text()
-        .expect("Cannot get body text from request");
+    loop{
+        //Do the things!
+        let _gaurd = purple_exporter.wait_duration(update_frequency);
 
-    info!("Exporter metrics:\n{}", body);
+        //Get a reading
+        let reading = get_reading(sensor_index, purple_read_key);
+
+        //Update Metrics
+    }
+
 }
