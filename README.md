8c8bf1b87e097d5c68a1c6f10853f6cdf3ddaa7d
Wayne Manselle <wayne@viridianforge.tech>
2021-04-24 19:33:37 -0700

    Ugly first working alpha
    Some more docs, heavy refactoring needed

--- README.md
+++ README.md
@@ -7,80 +7,45 @@ This project was developed so I could pipe the information from my own PAII
 sensor to a grafana chart I run in my house. In the future, I may expand the
 usability of this exporter to other PA sensors, or ranges of sensors.
 
-## Getting Started
+## Getting the Required API Key
 
-### What you'll need
 - A PurpleAir API Key
 
-Get a PurpleAir API key
+In order to use the exporter, one will need a PurpleAir API key. This can be
+obtained by e-mailing [contact@purpleair.com](mailto:contact@purpleair.com).
+Approved applications will receive a read key, and a write key. This exporter
+only makes use of the read key to retrieve information from the specified
+sensor.
 
-Configure environment variables
+## Running the Exporter
 
-## Use
+_Command Line Instructions go here_
 
-Launch the thing
+### Exposed Metrics
 
-Use the data
+Metrics are collected from all sensors for which they are available.
 
-https://api.purpleair.com/#api-welcome
+#### Environmental Data
 
-## Sample Response
-{
-  "api_version" : "V1.0.6-0.0.9",
-  "time_stamp" : 1617597434,
-  "sensor" : {
-    "sensor_index" : REDACTED,
-    "name" : "REDACTED",
-    "model" : "PA-II",
-    "location_type" : 0,
-    "latitude" : REDACTED,
-    "longitude" : REDACTED,
-    "altitude" : REDACTED,
-    "last_seen" : 1617597338,
-    "last_modified" : 1612417482,
-    "private" : 0,
-    "channel_state" : 3,
-    "channel_flags_manual" : 0,
-    "humidity_a" : 44,
-    "temperature_a" : 48.0,
-    "pressure_a" : 1013.42,
-    "pm1.0_a" : 0.77,
-    "pm1.0_b" : 0.64,
-    "pm2.5_a" : 1.71,
-    "pm2.5_b" : 1.05,
-    "pm10.0_a" : 2.71,
-    "pm10.0_b" : 1.32,
-    "0.3_um_count_a" : 431.79,
-    "0.3_um_count_b" : 473.08,
-    "0.5_um_count_a" : 123.86,
-    "0.5_um_count_b" : 120.76,
-    "1.0_um_count_a" : 21.04,
-    "1.0_um_count_b" : 12.14,
-    "2.5_um_count_a" : 2.43,
-    "2.5_um_count_b" : 1.37,
-    "5.0_um_count_a" : 1.5,
-    "5.0_um_count_b" : 0.22,
-    "10.0_um_count_a" : 1.11,
-    "10.0_um_count_b" : 0.0,
-    "stats_a" : {"pm2.5":1.71,"pm2.5_10minute":0.54,"pm2.5_30minute":0.57,"pm2.5_60minute":0.98,"pm2.5_6hour":6.6,"pm2.5_24hour":9.98,"pm2.5_1week":7.98,"time_stamp":1617597338},
-    "stats_b" : {"pm2.5":1.05,"pm2.5_10minute":0.56,"pm2.5_30minute":0.57,"pm2.5_60minute":0.86,"pm2.5_6hour":5.82,"pm2.5_24hour":9.0,"pm2.5_1week":7.29,"time_stamp":1617597338},
-    "analog_input" : 0.05,
-    "primary_id_a" : REDACTED,
-    "primary_key_a" : "REDACTED",
-    "secondary_id_a" : REDACTED,
-    "secondary_key_a" : "REDACTED",
-    "primary_id_b" : REDACTED,
-    "primary_key_b" : "REDACTED",
-    "secondary_id_b" : REDACTED,
-    "secondary_key_b" : "REDACTED",
-    "hardware" : "2.0+BME280+PMSX003-B+PMSX003-A",
-    "led_brightness" : 15.0,
-    "firmware_upgrade" : "6.01",
-    "firmware_version" : "6.01",
-    "rssi" : -84.0,
-    "icon" : 0,
-    "confidence_manual" : 100,
-    "confidence_auto" : 100,
-    "channel_flags_auto" : 0
-  }
-}
+- Humidity
+- Temperature
+- Air Pressure
+
+#### Estimated Mass Concentrations
+
+- pm1.0: Estimated mass concentration of particles less than 1 uM in diameter. (ug/m)
+- pm2.5: Estimated mass concentration of particles less than 2.5 uM in diameter. (ug/m)
+- pm10.0: Estimated mass concentration of particles less than 10.0 uM in diameter. (ug/m)
+
+#### Partical Concentrations
+
+- 0.3um: Count of particles greater than 0.3 uM in diameter. (particles/100mL)
+- 0.5um: Count of particles greater than 0.5 uM in diameter. (particles/100mL)
+- 1.0um: Count of particles greater than 1.0 uM in diameter. (particles/100mL)
+- 2.5um: Count of particles greater than 2.5 uM in diameter. (particles/100mL)
+- 5.0um: Count of particles greater than 5.0 uM in diameter. (particles/100mL)
+- 10.0um: Count of particles greater than 10.0 uM in diameter. (particles/100mL)
+
+## References
+
+[https://api.purpleair.com/#api-welcome](PurpleAir API docs)
--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,164 @@
-use env_logger::{
-    Builder,
-    Env
-};
-
-use log::info;
-use prometheus_exporter::prometheus::register_gauge_vec;
-use prometheus_exporter::prometheus::core::MetricVec;
+use prometheus_exporter::prometheus::register_gauge;
 use std::net::SocketAddr;
+
+use reqwest::header;
+use reqwest::blocking::Client;
+use url::Url;
 
 // Metrics - should be grabbed from a single API call. Calls should be limited
 // to only as often as Purple Requests (1-10 Minutes)
 // Make absolutely sure that requests for multiple sensors are fit into a single
 // request.
 
-// Configuration Required
-// API_READ_KEY
-// UPDATE_FREQUENCY (60s, 300s, 600s)
-// Humidity (sensor A only)
-// Temperature (sensor A only)
-// Pressure (sensor A only)
-// Analog_Input
-// SENSOR READINGS (For A and B)
-// pm1.0
-// pm2.5
-// pm10
-// 0.3_um_count
-// 0.5_um_count
-// 1.0_um_count
-// 2.5_um_count
-// 5.0_um_count
-// 10.0_um_count
+// Convert docs to MD
+struct Reading{
+    humidity: f32,
+    temperature: f32,
+    pressure: f32,
+    pm_sen_a: Vec<f32>,
+    pm_sen_b: Vec<f32>,
+    ct_sen_a: Vec<f32>,
+    ct_sen_b: Vec<f32>
+}
 
 // Retrieves a reading from the PurpleAPI sensor API end point
 // The response from this is used to update the outputs of the exporter
-fn get_reading(sensor_index:String, purple_read_key:String) -> String{
-    // Get Metrics
-    let apiResponse = reqwest::blocking::get(&format!("https://api.purpleair.com/v1/sensors/{}", sensor_index))
-    .expect("Response from PurpleAPI could not be retrieved.")
-    .text(); //Needs to be JSON
+fn get_reading(raw_resp:String) -> Reading{
+
+    // Convert Raw Response to JSON
+    let json_response = json::parse(&raw_resp).unwrap();
 
     //Need to convert this response into a proper form
-    apiResponse.unwrap()
+    let reading = Reading{
+        humidity: json_response["sensor"]["humidity_a"].as_f32().unwrap(),
+        temperature: json_response["sensor"]["temperature_a"].as_f32().unwrap(),
+        pressure: json_response["sensor"]["pressure_a"].as_f32().unwrap(),
+        pm_sen_a: vec![json_response["sensor"]["pm1.0_a"].as_f32().unwrap(),
+            json_response["sensor"]["pm2.5_a"].as_f32().unwrap(), 
+            json_response["sensor"]["pm10.0_a"].as_f32().unwrap()],
+        pm_sen_b: vec![json_response["sensor"]["pm1.0_b"].as_f32().unwrap(),
+            json_response["sensor"]["pm2.5_b"].as_f32().unwrap(), 
+            json_response["sensor"]["pm10.0_b"].as_f32().unwrap()],
+        ct_sen_a: vec![json_response["sensor"]["0.3_um_count_a"].as_f32().unwrap(),
+            json_response["sensor"]["0.5_um_count_a"].as_f32().unwrap(), 
+            json_response["sensor"]["1.0_um_count_a"].as_f32().unwrap(), 
+            json_response["sensor"]["2.5_um_count_a"].as_f32().unwrap(), 
+            json_response["sensor"]["5.0_um_count_a"].as_f32().unwrap(), 
+            json_response["sensor"]["10.0_um_count_a"].as_f32().unwrap()],
+        ct_sen_b: vec![json_response["sensor"]["0.3_um_count_b"].as_f32().unwrap(),
+            json_response["sensor"]["0.5_um_count_b"].as_f32().unwrap(), 
+            json_response["sensor"]["1.0_um_count_b"].as_f32().unwrap(), 
+            json_response["sensor"]["2.5_um_count_b"].as_f32().unwrap(), 
+            json_response["sensor"]["5.0_um_count_b"].as_f32().unwrap(), 
+            json_response["sensor"]["10.0_um_count_b"].as_f32().unwrap()],
+    };
+
+    return reading;
 }
 
 fn main() {
-    Builder::from_env(Env::default().default_filter_or("info")).init();
 
     //Set up configuration items
     let update_frequency = std::time::Duration::from_secs(300);
     let sensor_index = String::from("REDACTED");
     let purple_read_key = String::from("REDACTED");
 
+    // Build Headers
+    let mut headers = header::HeaderMap::new();
+    headers.insert("X-API-Key", header::HeaderValue::from_str(&purple_read_key).unwrap());
+
+    // Build Client
+    let client = Client::builder()
+        .default_headers(headers)
+        .build().unwrap();
 
     //Set up Exporter Address
     let raw_addr = "0.0.0.0:9184";
     let addr : SocketAddr = raw_addr.parse().expect("Listening Address Could Not Be Parsed.");
 
-    //Set up Metrics 
-    let env_gauge_vec = register_gauge_vec!("Environment_Gauge_Vec", "Sensor Environmental Data", &["Humidity", "Sensor Temp", "Pressure"])
-        .expect("Could not register environmental metrics as vec gauage.");
+    //Set up Metrics
+
+    // Atmospherics
+    let humidity_gauge = register_gauge!("Humidity", "Humidity Measured by Sensor").expect("Cannot create Humidity gauge.");
+    let temperature_gauge = register_gauge!("Temperature", "Temperature Measured by Sensor").expect("Cannot create Temperature gauge.");
+    let pressure_gauge = register_gauge!("Pressure", "Barometric Pressure Measured by Sensor").expect("Cannot create Barometric Pressure gauge.");
     
-    let pm_gauge_vec = register_gauge_vec!("PM_Gauge_Vec", "Sensor Estimated Mass Concentrations (ug/m^3)", &["pm1.0_a", "pm2.0_a", "pm10.0_a","pm1.0_b", "pm2.0_b", "pm10.0_b"])
-        .expect("Could not register mass concentration metrics as vec gauage.");
-    
-    let um_gauge_vec = register_gauge_vec!("UM_Gauge_Vec", "Sensor Particle Counts by Size", &["0.3_um_a", "0.5_um_a", "1.0_um_a","2.5_um_a", "5.0_um_a", "10.0_um_b", "0.3_um_b", "0.5_um_b", "1.0_um_b","2.5_um_b", "5.0_um_b", "10.0_um_b"])
-        .expect("Could not register particle counts as vec gauage.");
+    // Particle Concentrations
+    let mass_one_a = register_gauge!("PM_1_0_A", "Sensor A Estimated Concentration of Particles Less than 1 micron in diameter (ug/m^3)").expect("Cannot create 1 Micron Concentration gauge A.");
+    let mass_one_b = register_gauge!("PM_1_0_B", "Sensor B Estimated Concentration of Particles Less than 1 micron in diameter (ug/m^3)").expect("Cannot create 1 Micron Concentration gauge B.");
+    let mass_two_a = register_gauge!("PM_2_0_A", "Sensor A Estimated Concentration of Particles Less than 2 microns in diameter (ug/m^3)").expect("Cannot create 2 Micron Concentration gauge A.");
+    let mass_two_b = register_gauge!("PM_2_0_B", "Sensor B Estimated Concentration of Particles Less than 2 microns in diameter (ug/m^3)").expect("Cannot create 2 Micron Concentration gauge B.");
+    let mass_ten_a = register_gauge!("PM_10_0_A", "Sensor A Estimated Concentration of Particles Less than 10 microns in diameter (ug/m^3)").expect("Cannot create 10 Micron Concentration gauge A.");
+    let mass_ten_b = register_gauge!("PM_10_0_B", "Sensor B Estimated Concentration of Particles Less than 10 microns in diameter (ug/m^3)").expect("Cannot create 10 Micron Concentration gauge B.");
+
+    // Particle Counts
+    let count_pthree_a = register_gauge!("Count_0_3_A", "Sensor A Count of Particles less than 0.3 microns in diameter").expect("Cannot create 0.3 Micron Count gauge A.");
+    let count_pthree_b = register_gauge!("Count_0_3_B", "Sensor B Count of Particles less than 0.3 microns in diameter").expect("Cannot create 0.3 Micron Count gauge B.");
+    let count_pfive_a = register_gauge!("Count_0_5_A", "Sensor A Count of Particles less than 0.5 microns in diameter").expect("Cannot create 0.5 Micron Count gauge A.");
+    let count_pfive_b = register_gauge!("Count_0_5_B", "Sensor B Count of Particles less than 0.5 microns in diameter").expect("Cannot create 0.5 Micron Count gauge B.");
+    let count_one_a = register_gauge!("Count_1_0_A", "Sensor A Count of Particles less than 1.0 microns in diameter").expect("Cannot create 1.0 Micron Count gauge A.");
+    let count_one_b = register_gauge!("Count_1_0_B", "Sensor B Count of Particles less than 1.0 microns in diameter").expect("Cannot create 1.0 Micron Count gauge B.");
+    let count_twopfive_a = register_gauge!("Count_2_5_A", "Sensor A Count of Particles less than 2.5 microns in diameter").expect("Cannot create 2.5 Micron Count gauge A.");
+    let count_twopfive_b = register_gauge!("Count_2_5_B", "Sensor B Count of Particles less than 2.5 microns in diameter").expect("Cannot create 2.5 Micron Count gauge B.");
+    let count_five_a = register_gauge!("Count_5_0_A", "Sensor A Count of Particles less than 5.0 microns in diameter").expect("Cannot create 5.0 Micron Count gauge A.");
+    let count_five_b = register_gauge!("Count_5_0_B", "Sensor B Count of Particles less than 5.0 microns in diameter").expect("Cannot create 5.0 Micron Count gauge B.");
+    let count_ten_a = register_gauge!("Count_10_0_A", "Sensor A Count of Particles less than 10.0 microns in diameter").expect("Cannot create 10.0 Micron Count gauge A.");
+    let count_ten_b = register_gauge!("Count_10_0_B", "Sensor B Count of Particles less than 10.0 microns in diameter").expect("Cannot create 10.0 Micron Count gauge B.");
 
     //Start exporter
     let purple_exporter = prometheus_exporter::start(addr).expect("Cannot start exporter.");
 
     loop{
         //Do the things!
+        println!("Starting the loop!");
+
+        //Build URL used in GETs
+        let mut url_string = "https://api.purpleair.com/v1/sensors/".to_owned();
+        url_string.push_str(&sensor_index);
+
+        let sensor_url = Url::parse(&url_string).unwrap();
+
+        println!("Attempting to get a reading.");
+
+        //Get a reading        
+        let raw_resp = client.get(sensor_url).send()
+            .expect("Response from PurpleAPI could not be retrieved.")
+            .text()
+            .expect("Unable to unwrap response");
+
+        println!("{}", raw_resp);
+
+        let reading = get_reading(raw_resp);
+
+        //Update Atmospherics
+        humidity_gauge.set(reading.humidity.into());
+        temperature_gauge.set(reading.temperature.into());
+        pressure_gauge.set(reading.pressure.into());
+
+        //Update Mass Concentrations
+        mass_one_a.set(reading.pm_sen_a[0].into());
+        mass_one_b.set(reading.pm_sen_b[0].into());
+        mass_two_a.set(reading.pm_sen_a[1].into());
+        mass_two_b.set(reading.pm_sen_b[1].into());
+        mass_ten_a.set(reading.pm_sen_a[2].into());
+        mass_ten_b.set(reading.pm_sen_b[2].into());
+
+        //Update Particle Counts
+        count_pthree_a.set(reading.ct_sen_a[0].into());
+        count_pthree_b.set(reading.ct_sen_b[0].into());
+        count_pfive_a.set(reading.ct_sen_a[1].into());
+        count_pfive_b.set(reading.ct_sen_b[1].into());
+        count_one_a.set(reading.ct_sen_a[2].into());
+        count_one_b.set(reading.ct_sen_b[2].into());
+        count_twopfive_a.set(reading.ct_sen_a[3].into());
+        count_twopfive_b.set(reading.ct_sen_b[3].into());
+        count_five_a.set(reading.ct_sen_a[4].into());
+        count_five_b.set(reading.ct_sen_b[4].into());
+        count_ten_a.set(reading.ct_sen_a[5].into());
+        count_ten_b.set(reading.ct_sen_b[5].into());
+
+        println!("Wait until next update.");
         let _gaurd = purple_exporter.wait_duration(update_frequency);
-
-        //Get a reading
-        let reading = get_reading(sensor_index, purple_read_key);
-
-        //Update Metrics
     }
 
 }
